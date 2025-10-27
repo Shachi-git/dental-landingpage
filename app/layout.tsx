ad7f59a17f5e046e2054c730d319257ebc1fb3d2
Shachi-git <delacruz.johnpaulstar@gmail.com>
2025-10-27 16:34:26 +0800

    feat: add Google Tag Manager and jQuery script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { TopBar } from '../components/TopBar'
 import Footer from '@/components/Footer'
@@ -28,6 +29,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script
+          src="https://code.jquery.com/jquery-3.6.0.min.js"
+          strategy="beforeInteractive"
+        />
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PQMLF23');
+    `}
+        </Script>
+      </head>
+
       <body
         className={`
           ${geistSans.variable} 
@@ -38,6 +57,16 @@ export default function RootLayout({
           w-full
         `}
       >
+        {/*Google Tag Manager (noscript)*/}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQMLF23"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <div className="flex flex-col min-h-screen max-w-full overflow-x-hidden">
           <TopBar />
           <main className="flex-grow w-full max-w-full overflow-x-hidden">
